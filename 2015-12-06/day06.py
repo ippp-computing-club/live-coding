1cf5a987483dac08f79337242d8c35ade99a2409
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2023-08-22 13:22:47 +0100

    day 6: main logic

--- 2015-12-06/day06.py
+++ 2015-12-06/day06.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-grid = np.zeros((1000,1000))
+grid = np.zeros((1000,1000), dtype=bool)
 
 with open('input06.txt') as fp:
     inp = fp.read().split('\n')
@@ -16,8 +16,8 @@ for line in inp:
     l, t = parse_coord(words[-3])
     r, b = parse_coord(words[-1])
     if line.startswith("turn on"):
-        pass
+        grid[t:b+1, l:r+1] = True
     elif line.startswith("turn off"):
-        pass
+        grid[t:b+1, l:r+1] = False
     elif line.startswith("toggle"):
-        pass
+        grid[t:b+1, l:r+1] = ~grid[t:b+1, l:r+1]
