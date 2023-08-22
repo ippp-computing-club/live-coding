4ff9bc16e1f97b5770528480613afd799d734d65
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2023-08-22 13:19:48 +0100

    day 6: added parser

--- 2015-12-06/day06.py
+++ 2015-12-06/day06.py
@@ -7,10 +7,17 @@ with open('input06.txt') as fp:
     inp = fp.read().split('\n')
 
 
+def parse_coord(s):
+    return (int(i) for i in s.split(','))
+
+
 for line in inp:
+    words = line.split(' ')
+    l, t = parse_coord(words[-3])
+    r, b = parse_coord(words[-1])
     if line.startswith("turn on"):
         pass
     elif line.startswith("turn off"):
         pass
-    elif line.startswith("turn toggle"):
+    elif line.startswith("toggle"):
         pass
